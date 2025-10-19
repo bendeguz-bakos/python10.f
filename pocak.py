cb2984542a1873630538951b5a6088d996164ac2
benthegoose <bendebakos@gmail.com>
2025-10-19 14:52:36 +0200

    .

--- pocak.py
+++ pocak.py
@@ -1,5 +1,5 @@
 import math
-x = int(input("ird be a pocak hosszusaganak merteket: "))
+x = int(input("ird be a pocak hosszusaganak merteket:  "))
 for i in range(math.floor(x/2)):
     print('*' * (i+1))
 for j in range(math.ceil(x/2)):
