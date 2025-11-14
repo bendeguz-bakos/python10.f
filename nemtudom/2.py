95d3c58b4220f9c8eca551331399e8896a760bd4
bendeguz <bendebakos@gmail.com>
2025-11-14 09:47:15 +0100

    asd

--- nemtudom/2.py
+++ nemtudom/2.py
@@ -4,6 +4,7 @@ for i in range(10):
     list.append(rnd.randint(1,3))
 szam = int(input("Kérek egy számot 1 és 3 között: "))
 if szam in list:
-    list.remove(szam)
+    while szam in list:
+        list.remove(szam)
     print(f"A szám szerepelt a listában, eltávolítottam: {szam}")
 print(list)
