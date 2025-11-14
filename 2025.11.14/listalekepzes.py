62aa5170cbfad6e72d50e05595b01aec24541054
bendeguz <bendebakos@gmail.com>
2025-11-14 10:08:22 +0100

    Add vmi.py and listalekepzes.py with list processing examples

---
+++ 2025.11.14/listalekepzes.py
@@ -0,0 +1,9 @@
+text =["apple", "banana", "cherry", "dragonfruit", "elderberry", "oh","f"]
+
+for i in text:
+    if len(i)>3:
+        print(i)
+nagytext_list = [szo.upper for szo in text if len(szo)>3]
+    
+        
+
---
+++ 2025.11.14/vmi.py
@@ -0,0 +1,4 @@
+lista = [1,2,3,4,5,6,78,676767]
+
+eredmeny = [x*2 for x in lista]
+print(eredmeny)
