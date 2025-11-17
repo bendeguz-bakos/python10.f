0480793ad6fd2cebed34b4e7289eb3d328f93ca6
bendeguz <bendebakos@gmail.com>
2025-11-17 22:41:39 +0100

    a

---
+++ .history/hazinov17_20251117224036.py
@@ -0,0 +1,5 @@
+string_list = ["apple","banana","cherry","date","elderberry","fig","grape","honeydew","kiwi","lemon","mango","nectarine","orange","papaya","quince","raspberry","strawberry","tangerine","ugli fruit","voavanga","watermelon","xigua","yellow passion fruit","zucchini"]
+ujlista = [ x.capitalize() for x in string_list]
+
+print(ujlista)
+print(string_list)
