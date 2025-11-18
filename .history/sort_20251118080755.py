f2b6f0c1fc6123c644078c2296c7ceb7108f67b1
bakos_bendeguz <bendebakos@gmail.com>
2025-11-18 08:13:33 +0100

    sort

---
+++ .history/sort_20251118080755.py
@@ -0,0 +1,25 @@
+string_list = ["apple","banana","cherry","date","elderberry","agartha"]
+
+for szo in string_list:
+    print(szo)
+    
+for karakter in 'apple':
+    print(karakter)
+
+for index, karakter in enumerate('banana', start=2):
+    print(f'{index}: {karakter}')
+
+for _ in range(1,5,2):
+    print("alma")
+
+for szo in string_list:
+    if szo.startswith(('a')):
+        continue
+    print(szo)    
+        
+i = 0
+while i < len(string_list):
+    if string_list[i].startswith('a'):
+        string_list[i]="törlés"
+    i += 1                   
+print(string_list)    
