1dd01bf0236b7ca037ae80b63ecd455a64ffce67
bakos_bendeguz <bendebakos@gmail.com>
2025-11-18 07:49:52 +0100

    asd

--- hazinov17.py
+++ hazinov17.py
@@ -1,5 +1,6 @@
 string_list = ["apple","banana","cherry","date","elderberry","fig","grape","honeydew","kiwi","lemon","mango","nectarine","orange","papaya","quince","raspberry","strawberry","tangerine","ugli fruit","voavanga","watermelon","xigua","yellow passion fruit","zucchini"]
 ujlista = [ x.capitalize() for x in string_list]
 
-print(ujlista)
-print(string_list)
+#print(ujlista)
+print((" & ").join(ujlista))
+#print(string_list)
